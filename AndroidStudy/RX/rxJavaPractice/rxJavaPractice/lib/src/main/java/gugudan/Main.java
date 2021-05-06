316cdf8a43b543192ff0c12cb1737c340ca0314f
jaewonkim <turitus@naver.com>
2021-05-06 22:49:50 +0900

    [RxJava]구구단 연습

---
+++ AndroidStudy/RX/rxJavaPractice/rxJavaPractice/lib/src/main/java/gugudan/Main.java
@@ -0,0 +1,16 @@
+package gugudan;
+
+import java.util.Scanner;
+import io.reactivex.Observable;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Gugudan Input:");
+		int dan = Integer.parseInt(in.nextLine());
+		
+		Observable<Integer> source = Observable.range(1,9);
+		source.subscribe(row -> System.out.println(dan + " * "+row+" = "+dan * row));
+	}
+	
+}
