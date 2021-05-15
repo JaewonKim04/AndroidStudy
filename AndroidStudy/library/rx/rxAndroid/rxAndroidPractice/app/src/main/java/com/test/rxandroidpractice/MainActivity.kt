be6de05f3c421e2d6e6359376c8c2266244fd8a5
jaewonkim <turitus@naver.com>
2021-05-15 23:34:18 +0900

    [RxAndroid]예제 수정

--- AndroidStudy/library/rx/rxAndroid/rxAndroidPractice/app/src/main/java/com/test/rxandroidpractice/MainActivity.kt
+++ AndroidStudy/library/rx/rxAndroid/rxAndroidPractice/app/src/main/java/com/test/rxandroidpractice/MainActivity.kt
@@ -6,9 +6,14 @@ import com.test.rxandroidpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var mainActivityBinding:ActivityMainBinding
+    val reactiveTool = ReactiveTool()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainActivityBinding.root)
+
+        val textView = mainActivityBinding.watcherTv
+        val editText = mainActivityBinding.inputEt
+        reactiveTool.setTextView(textView,editText)
     }
 }
--- AndroidStudy/library/rx/rxAndroid/rxAndroidPractice/app/src/main/java/com/test/rxandroidpractice/ReactiveTool.kt
+++ AndroidStudy/library/rx/rxAndroid/rxAndroidPractice/app/src/main/java/com/test/rxandroidpractice/ReactiveTool.kt
@@ -2,20 +2,20 @@ package com.test.rxandroidpractice
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.TextView
-import com.google.android.material.textfield.TextInputEditText
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableEmitter
-import io.reactivex.rxjava3.core.ObservableOnSubscribe
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 class ReactiveTool {
-    fun setTextView(textView: TextView,editText: TextInputEditText){
+    fun setTextView(textView: TextView,editText: EditText){
         val observableTextQuery = Observable
-            .create(ObservableOnSubscribe { emitter: ObservableEmitter<String>? ->
+            .create { emitter: ObservableEmitter<String>? ->
                 editText.addTextChangedListener(object : TextWatcher {
                     override fun afterTextChanged(s: Editable?) {
                     }
@@ -26,20 +26,26 @@ class ReactiveTool {
                         count: Int,
                         after: Int
                     ) {
+                    }
+
+                    override fun onTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        before: Int,
+                        count: Int
+                    ) {
                         emitter?.onNext(s.toString())
                     }
 
-                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    }
-
                 })
-            })
+            }
+            .filter { it != null }
             .map{changeText(it)}
             .debounce(500, TimeUnit.MILLISECONDS)
             .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
         observableTextQuery.subscribe(object : Observer<String> {
             override fun onSubscribe(d: Disposable?) {
-                TODO("Not yet implemented")
             }
 
             override fun onNext(t: String?) {
@@ -47,16 +53,14 @@ class ReactiveTool {
             }
 
             override fun onError(e: Throwable?) {
-                TODO("Not yet implemented")
             }
 
             override fun onComplete() {
-                TODO("Not yet implemented")
             }
         })
     }
 
     private fun changeText(text:String):String{
-        return text + "가 입력되었습니다"
+        return "$text 이(가) 입력되었습니다"
     }
 }
