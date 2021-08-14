55e5b5098f942b8eda995671d15d655fce1d952e
JaeWonKim <turitus@naver.com>
2021-08-14 16:47:09 +0900

    [COMPOSE]card view

--- AndroidStudy/compose/Tutorial/app/src/main/java/com/compose/tutorial/MainActivity.kt
+++ AndroidStudy/compose/Tutorial/app/src/main/java/com/compose/tutorial/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.compose.tutorial
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.compose.tutorial.ui.theme.TutorialTheme
@@ -22,11 +27,13 @@ class MainActivity : ComponentActivity() {
 
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Column(modifier = Modifier.fillMaxSize()) {
+                    Column(modifier = Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally) {
                         ColumnText("Android")
                         RowText()
+                        PersonCard(person = Person("Tester",17))
                     }
                 }
+
             }
         }
     }
@@ -45,10 +52,47 @@ fun ColumnText(name: String) {
 
 @Composable
 fun RowText() {
-    Row (modifier = Modifier.fillMaxWidth().padding(20.dp),horizontalArrangement = Arrangement.SpaceEvenly){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp), horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
         Text(text = "Row")
         Text(text = "Test")
     }
+}
+
+@Composable
+fun PersonCard(person: Person) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(5.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Person profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            SetNameText(name = person.name)
+            Spacer(modifier = Modifier.height(4.dp))
+            SetAgeText(age = person.age)
+        }
+    }
+}
+
+@Composable
+fun SetAgeText(age:Int){
+    Text(text = "나이: $age 세")
+}
+
+@Composable
+fun SetNameText(name:String){
+    Text(text = "이름: $name")
 }
 
 @Preview(showBackground = true)
@@ -57,11 +101,8 @@ fun DefaultPreview() {
     TutorialTheme {
 
         // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background,modifier = Modifier.fillMaxSize()) {
-            Column(modifier = Modifier.fillMaxSize()) {
-                ColumnText("Android")
-                RowText()
-            }
+        Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
+            PersonCard(person = Person("Test",18))
         }
     }
 }
---
+++ AndroidStudy/compose/Tutorial/app/src/main/java/com/compose/tutorial/Person.kt
@@ -0,0 +1,3 @@
+package com.compose.tutorial
+
+data class Person(val name: String, val age: Int)
