ad67dcffcb1c050ecc97e557d73afb5ca06d4567
JaeWonKim <turitus@naver.com>
2021-08-16 19:54:43 +0900

    [COMPOSE] compose mvvm clicker game

--- AndroidStudy/compose/ClickerGame/app/src/main/java/com/compose/clickergame/MainActivity.kt
+++ AndroidStudy/compose/ClickerGame/app/src/main/java/com/compose/clickergame/MainActivity.kt
@@ -3,31 +3,37 @@ package com.compose.clickergame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.compose.clickergame.ui.theme.ClickerGameTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val vm = ViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ClickerGameTheme {
-                // A surface container using the 'background' color from the theme
+                val score = vm.score.observeAsState().value
                 Surface(color = MaterialTheme.colors.background) {
                     Column(
                         modifier = Modifier.fillMaxSize(),
                         horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
+                        verticalArrangement = Arrangement.SpaceEvenly
                     ) {
-                        ScoreText()
+                        ScoreText(score?:0)
+                        ScoreButtons(vm = vm)
                     }
                 }
             }
@@ -37,12 +43,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ScoreText(score: Int) {
+    Text(text = score.toString(),fontSize = 20.sp)
+}
 
+@Composable
+fun ScoreButtons(vm:ViewModel){
+    Row(horizontalArrangement = Arrangement.SpaceEvenly,modifier = Modifier.fillMaxWidth()) {
+        Button(onClick = { vm.plusScore() }) {
+            Text(text = "Plus")
+        }
+        Button(onClick = { vm.resetScore() }) {
+            Text(text = "Reset")
+        }
+        Button(onClick = { vm.minusScore() }) {
+            Text(text = "Minus")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ClickerGameTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                ScoreText(0)
+                ScoreButtons(ViewModel())
+            }
+        }
     }
 }
--- AndroidStudy/compose/ClickerGame/app/src/main/java/com/compose/clickergame/ViewModel.kt
+++ AndroidStudy/compose/ClickerGame/app/src/main/java/com/compose/clickergame/ViewModel.kt
@@ -1,7 +1,23 @@
 package com.compose.clickergame
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ViewModel:ViewModel() {
 
+    private val _score = MutableLiveData(1)
+    val score : LiveData<Int> get() = _score
+
+    fun plusScore(){
+        _score.value = score.value!!+1
+    }
+
+    fun resetScore(){
+        _score.value = 1
+    }
+
+    fun minusScore(){
+        _score.value = score.value!!-1
+    }
 }
